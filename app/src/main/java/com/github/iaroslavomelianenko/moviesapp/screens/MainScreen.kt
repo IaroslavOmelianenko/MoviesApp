f853da741743076876b039a2df9bcd3c1572108f
IAROSLAV OMELIANENKO <om-y@yandex.ru>
2023-03-27 23:13:59 +0300

    add nav, main screen, splash screen

---
+++ app/src/main/java/com/github/iaroslavomelianenko/moviesapp/screens/MainScreen.kt
@@ -0,0 +1,17 @@
+package com.github.iaroslavomelianenko.moviesapp.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainScreen() {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Text(text = "Main screen")
+    }
+}
---
+++ app/src/main/java/com/github/iaroslavomelianenko/moviesapp/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.github.iaroslavomelianenko.moviesapp.utils
+
+class Constants {
+
+    object Screens {
+        const val SPLASH_SCREEN = "splash_screen"
+        const val MAIN_SCREEN = "MAIN_SCREEN"
+        const val DETAILS_SCREEN = "DETAILS_SCREEN"
+    }
+}
